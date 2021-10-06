f6f3fc2819af15b8fa8ca1e9221d93a107e21f78
Erick Fariñas <efarinas99@gmail.com>
2021-10-06 16:02:05 -0600

    connect ui and setup internet permission

--- app/src/main/java/com/farinas/cryptocompose/presentation/MainActivity.kt
+++ app/src/main/java/com/farinas/cryptocompose/presentation/MainActivity.kt
@@ -5,11 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.farinas.cryptocompose.common.Constants
+import com.farinas.cryptocompose.presentation.coin_detail.CoinDetailScreen
+import com.farinas.cryptocompose.presentation.coin_list.CoinListScreen
 import com.farinas.cryptocompose.presentation.ui.theme.CryptoComposeTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,22 +22,24 @@ class MainActivity : ComponentActivity() {
             CryptoComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.CoinListScreen.route
+                    ) {
+                        composable(
+                            route = Screen.CoinListScreen.route
+                        ) {
+                            CoinListScreen(navController)
+                        }
+                        composable(
+                            route = Screen.CoinDetailScreen.route + "/{${Constants.PARAM_COIN_ID}}"
+                        ) {
+                            CoinDetailScreen()
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CryptoComposeTheme {
-        Greeting("Android")
     }
 }
