c6c2e777efac094172117b2d7850dba45caa65c0
Erick Fariñas <efarinas99@gmail.com>
2021-10-05 18:49:17 -0600

    setup colors and theme

--- app/src/main/java/com/farinas/cryptocompose/ui/theme/Theme.kt
+++ app/src/main/java/com/farinas/cryptocompose/ui/theme/Theme.kt
@@ -7,24 +7,17 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = ColorPrimary,
+    background = DarkGray,
+    onBackground = TextWhite,
+    onPrimary = DarkGray
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
+    primary = ColorPrimary,
     background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    onBackground = MediumGray,
+    onPrimary = DarkGray
 )
 
 @Composable
